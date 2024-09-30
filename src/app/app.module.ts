c2c30e5dc7ddfbca1d08cc0cc0af6104a6ddd5ba
liuyuntian <liuyuntian@shinetechsoftware.com>
2024-09-30 15:10:48 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SelectInputFilterableModule } from "./modules/select-input-filter/select-input-filterable.module";
 import { AppComponent } from './app.component';
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SelectInputFilterableModule,
+    MatSelectModule,
+    MatOptionModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
